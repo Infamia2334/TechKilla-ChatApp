483781ed45ba47e421d09fee717b0477129e9f26
Infamia2334 <53487995+Infamia2334@users.noreply.github.com>
2021-07-03 02:17:54 +0530

    Set up message form and emitted msg to server-side js

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,11 @@
-io()
+const socket = io()
+
+socket.on("message", (welcome)=>{
+    console.log(welcome)
+})
+
+document.querySelector("#msg-form").addEventListener("submit", (e)=>{
+    e.preventDefault()
+    const message = document.querySelector("input").value
+    socket.emit("sendMessage", message)
+})
