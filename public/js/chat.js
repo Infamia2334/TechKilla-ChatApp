fcead84fd58fde3145532dc420c19a22fdf8509b
Infamia2334 <53487995+Infamia2334@users.noreply.github.com>
2021-07-03 02:58:06 +0530

    Rendering messages in div using mustache

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,11 +1,19 @@
 const socket = io()
 
+const $messages = document.querySelector("#messages")
+
+const messageTemplate = document.querySelector("#message-template").innerHTML
+
 socket.on("message", (welcome)=>{
     console.log(welcome)
+    const html = Mustache.render(messageTemplate, {
+        message: welcome
+    })
+    $messages.insertAdjacentHTML("beforeend", html)
 })
 
 document.querySelector("#msg-form").addEventListener("submit", (e)=>{
     e.preventDefault()
-    const message = document.querySelector("input").value
+    const message = e.target.elements.message.value
     socket.emit("sendMessage", message)
 })
